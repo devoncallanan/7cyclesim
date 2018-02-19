7b6709a74c2dee002c6ef8688acea4aa24b08ddc
Michael Ing <mci10@pitt.edu>
2018-02-19 16:09:49 -0500

    got rid of debugging stuff, cleaned up braces

--- CPU.c
+++ CPU.c
@@ -51,7 +51,6 @@ int main(int argc, char **argv)
    **IF1 | IF2 | ID | EX | MEM1 | MEM2 | WB
    **/
   struct trace_item* pipeline[7];
-  //Changed by jen ####################################################################
   pipeline[0] = &noop;
   pipeline[1] = &noop;
   pipeline[2] = &noop;
@@ -59,14 +58,13 @@ int main(int argc, char **argv)
   pipeline[4] = &noop;
   pipeline[5] = &noop;
   pipeline[6] = &noop;
-  // ####################################################################
   
   int pipe_occupancy = 7;
   int prediction_method = 0;
   unsigned int cycle_number = 0;
   int stalled = NO_HAZ, squashed = NO_HAZ;
   int num_squash = 0;
-  unsigned int bp_hash_table[128][2];	//should this be 64? i wanted to not have to change the size of the array, the bit shifting will allow only 64 hash locations
+  unsigned int bp_hash_table[128][2];
   unsigned int hash_table_size = 64;
   
   //Other variable declarations
@@ -190,12 +188,9 @@ int main(int argc, char **argv)
 			 }
 		 } 
 	 } 
-
-	 //printf("cont hazard %d", squashed);
 	 
 	 //branch
      if (pipeline[0]->type == ti_BRANCH) {
-     //printf("in branch loop ");
 	   branch_taken = pipeline[0]->Addr == tr_entry->PC;
 	   index = (pipeline[0]->Addr >> 3 ) % hash_table_size; //if you want bits 8-3 should this be left 24 and right 27? probably...gonna use mod to pick bits now
 	   if (prediction_method == 0) {
@@ -211,7 +206,6 @@ int main(int argc, char **argv)
 				   squashed = CONT_HAZ;
 				   num_squash = 3;
 				   bp_hash_table[index][1] = branch_taken;
-				   //printf("branchhaz 1.0");
 			   }
 	       }
 		   else {
@@ -219,8 +213,7 @@ int main(int argc, char **argv)
 			   bp_hash_table[index][1] = branch_taken;
 			   if (branch_taken) {
 				   squashed = CONT_HAZ;
-				   num_squash = 3;			
-				   //printf("branchhaz 1.1");	   
+				   num_squash = 3;				   
 			   }
 		   }
 		   
@@ -267,15 +260,7 @@ int main(int argc, char **argv)
 			   }			   
 		   }
 	   }
-	 
 	 }
-	 	 //printf("cont hazard %d", squashed);
-
-
-	
-
-// SIMULATION OF A SINGLE CYCLE cpu IS TRIVIAL - EACH INSTRUCTION IS EXECUTED
-// IN ONE CYCLE
 
     /**
 	--From the assignment (page 3)
@@ -285,98 +270,47 @@ int main(int argc, char **argv)
 	*/
 	
     if (trace_view_on) {/* print the executed instruction if trace_view_on=1	*/
-	  if(pipeline[6] != NULL)
-	  {
-		  switch(pipeline[6]->type) {
-			case ti_NOP:
-			  if(pipeline[6]->dReg == 255) {
-				printf("[cycle %d] NOP\n",cycle_number) ;
-			  }
-			  else printf("[cycle %d] SQUASHED\n", cycle_number) ;
-			  break;
-			case ti_RTYPE:
-			  printf("[cycle %d] RTYPE:",cycle_number) ;
-			  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->dReg);
-			  break;
-			case ti_ITYPE:
-			  printf("[cycle %d] ITYPE:",cycle_number) ;
-			  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
-			  break;
-			case ti_LOAD:
-			  printf("[cycle %d] LOAD:",cycle_number) ;      
-			  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
-			  break;
-			case ti_STORE:
-			  printf("[cycle %d] STORE:",cycle_number) ;      
-			  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->Addr);
-			  break;
-			case ti_BRANCH:
-			  printf("[cycle %d] BRANCH:",cycle_number) ;
-			  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->Addr);
-			  break;
-			case ti_JTYPE:
-			  printf("[cycle %d] JTYPE:",cycle_number) ;
-			  printf(" (PC: %x)(addr: %x)\n", pipeline[6]->PC,pipeline[6]->Addr);
-			  break;
-			case ti_SPECIAL:
-			  printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
-			  break;
-			case ti_JRTYPE:
-			  printf("[cycle %d] JRTYPE:",cycle_number) ;
-			  printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->dReg, pipeline[6]->Addr);
-			  break;
+	  switch(pipeline[6]->type) {
+		case ti_NOP:
+		  if(pipeline[6]->dReg == 255) {
+			printf("[cycle %d] NOP\n",cycle_number) ;
 		  }
+		  else printf("[cycle %d] SQUASHED\n", cycle_number) ;
+		  break;
+		case ti_RTYPE:
+		  printf("[cycle %d] RTYPE:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->dReg);
+		  break;
+		case ti_ITYPE:
+		  printf("[cycle %d] ITYPE:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
+		  break;
+		case ti_LOAD:
+		  printf("[cycle %d] LOAD:",cycle_number) ;      
+		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
+		  break;
+		case ti_STORE:
+		  printf("[cycle %d] STORE:",cycle_number) ;      
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->Addr);
+		  break;
+		case ti_BRANCH:
+		  printf("[cycle %d] BRANCH:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->Addr);
+		  break;
+		case ti_JTYPE:
+		  printf("[cycle %d] JTYPE:",cycle_number) ;
+		  printf(" (PC: %x)(addr: %x)\n", pipeline[6]->PC,pipeline[6]->Addr);
+		  break;
+		case ti_SPECIAL:
+		  printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
+		  break;
+		case ti_JRTYPE:
+		  printf("[cycle %d] JRTYPE:",cycle_number) ;
+		  printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->dReg, pipeline[6]->Addr);
+		  break;
 	  }
     }
-    
-    //printf("\n haz on last inst: %d", stalled | squashed);
-	
-	/*
-	//print pipeline, DEBUGGING	
-	printf("\n[cycle %d]\n ",cycle_number) ;	
-	for (j = 0; j <= 6; j++)
-	{
-		switch(pipeline[j]->type) {
-        case ti_NOP:
-         if(pipeline[j]->dReg == 255) {
-			printf("[cycle %d] NOP\n",cycle_number) ;
-			}
-		 else printf("[cycle %d] SQUASHED\n", cycle_number) ;
-	     break;
-        case ti_RTYPE:
-          printf("RTYPE:") ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", pipeline[j]->PC, pipeline[j]->sReg_a, pipeline[j]->sReg_b, pipeline[j]->dReg);
-          break;
-        case ti_ITYPE:
-          printf("ITYPE:") ;
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[j]->PC, pipeline[j]->sReg_a, pipeline[j]->dReg, pipeline[j]->Addr);
-          break;
-        case ti_LOAD:
-          printf("LOAD:") ;      
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[j]->PC, pipeline[j]->sReg_a, pipeline[j]->dReg, pipeline[j]->Addr);
-          break;
-        case ti_STORE:
-          printf("STORE:") ;      
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[j]->PC, pipeline[j]->sReg_a, pipeline[j]->sReg_b, pipeline[j]->Addr);
-          break;
-        case ti_BRANCH:
-          printf("BRANCH:") ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[j]->PC, pipeline[j]->sReg_a, pipeline[j]->sReg_b, pipeline[j]->Addr);
-          break;
-        case ti_JTYPE:
-          printf("JTYPE:") ;
-          printf(" (PC: %x)(addr: %x)\n", pipeline[j]->PC,pipeline[j]->Addr);
-          break;
-        case ti_SPECIAL:
-          printf("SPECIAL:\n") ;      	
-          break;
-        case ti_JRTYPE:
-          printf("JRTYPE:") ;
-          printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", pipeline[j]->PC, pipeline[j]->dReg, pipeline[j]->Addr);
-          break;
-		}
-	}
-	*/
+
 	//Pipeline advancing loop
 	for (i = 6; i >= 1; i = i - 1)
 	{
@@ -412,8 +346,6 @@ int main(int argc, char **argv)
 	else if(!stalled) {
 		pipeline[0] = tr_entry;
 	}
-	//printf("Current instruction: Type: %d (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", pipeline[0]->type, pipeline[0]->PC, pipeline[0]->sReg_a, pipeline[0]->sReg_b, pipeline[0]->dReg);
-	//getchar();
   }
 
   trace_uninit();
