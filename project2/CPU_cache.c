f93ebf115f08d54311d6b94f1fe66005357c7601
devoncallanan <devoncallanan@gmail.com>
2018-03-26 16:23:11 -0400

    L1 almost working, data not doing all accesses

--- project2/CPU_cache.c
+++ project2/CPU_cache.c
@@ -1,9 +1,9 @@
 /**************************************************************/
-/* CS/COE 1541				 			
-   just compile with gcc -o pipeline pipeline.c			
-   and execute using							
-   ./pipeline  /afs/cs.pitt.edu/courses/1541/short_traces/sample.tr	0  
-   
+/* CS/COE 1541
+   just compile with gcc -o pipeline pipeline.c
+   and execute using
+   ./pipeline  /afs/cs.pitt.edu/courses/1541/short_traces/sample.tr	0
+
    code by Devon Callanan, Jen Dudek and Michael Ing
 ***************************************************************/
 
@@ -11,7 +11,7 @@
 #include <inttypes.h>
 #include <arpa/inet.h>
 #include <math.h>
-#include "CPU.h" 
+#include "CPU.h"
 #include "cache.h"
 
 enum hazard_type {
@@ -19,7 +19,7 @@ enum hazard_type {
 	STRUCT_HAZ,
 	DATA_HAZ,
 	CONT_HAZ,
-	INST_ACC, 
+	INST_ACC,
 	DATA_ACC
 };
 
@@ -39,19 +39,19 @@ int main(int argc, char **argv)
   size_t size = 0;
   char *trace_file_name;
   int trace_view_on = 0;
-  
+
   //constant noop that can be used to insert when stalling
   struct trace_item noop = {.type = ti_NOP, .sReg_a = 255, .sReg_b = 255, .dReg = 255, .PC = 0, .Addr = 0};
   struct trace_item squash = {.type = ti_NOP, .sReg_a = 255, .sReg_b = 255, .dReg = 0, .PC = 0, .Addr = 0}; //difference for squashed is that dReg is 0
-  
+
   unsigned char t_type = 0;
   unsigned char t_sReg_a= 0;
   unsigned char t_sReg_b= 0;
   unsigned char t_dReg= 0;
   unsigned int t_PC = 0;
   unsigned int t_Addr = 0;
-  
-  /**Array of trace items that represents the pipeline 
+
+  /**Array of trace items that represents the pipeline
    **Each index represents a stage:
    ** 0     1    2    3     4      5     6
    **IF1 | IF2 | ID | EX | MEM1 | MEM2 | WB
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
   pipeline[4] = &noop;
   pipeline[5] = &noop;
   pipeline[6] = &noop;
-  
+
   int pipe_occupancy = 7;
   int prediction_method = 0;
   unsigned int cycle_number = 0;
@@ -72,29 +72,31 @@ int main(int argc, char **argv)
   int num_squash = 0;
   unsigned int bp_hash_table[128][2];
   unsigned int hash_table_size = 32;
-  
+
   //Other variable declarations
   unsigned char tempWB, tempID;
   int data_haz_type = 0;
   int branch_taken;
-  char index;  
+  char index;
   int i = 0, j = 0;
-  
+
   //Cache variable declarations
-  struct cache_t *L1_I = cache_create(4, 16, 1, 80);
-  struct cache_t *L1_D = cache_create(4, 16, 1, 80);
+  struct cache_t *L1_I = cache_create(4, 16, 4, 80);
+  struct cache_t *L1_D = cache_create(4, 16, 4, 80);
   struct cache_t *L2 = cache_create(0, 1, 1, 1);
   //struct cache_t *L2 = cache_create(512, 16, 4, 80);
   int total_latency = 0;
   int cache_delay = NO_HAZ;
   int accesstype = 0; //denotes what type of access to memory is being made, 0 for memread and 1 for memwrite
-  
+
   unsigned int L1_I_accesses = 0;
   unsigned int L1_I_hits = 0;
   unsigned int L1_I_misses = 0;
   unsigned int L1_D_accesses = 0;
   unsigned int L1_D_hits = 0;
   unsigned int L1_D_misses = 0;
+	unsigned int D_inst_checked = 0;
+	unsigned int I_inst_checked = -1;
   float L1_I_missrate = 0;
   float L1_D_missrate = 0;
   float L2_missrate = 0;
@@ -104,7 +106,7 @@ int main(int argc, char **argv)
     fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
     exit(0);
   }
-    
+
   trace_file_name = argv[1];
   if (argc == 4){
 	  trace_view_on = atoi(argv[3]) ;
@@ -113,8 +115,8 @@ int main(int argc, char **argv)
   else if (argc == 3) {
 	  prediction_method = atoi(argv[2]);
   }
-  
-  
+
+
   fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
 
   trace_fd = fopen(trace_file_name, "rb");
@@ -127,11 +129,11 @@ int main(int argc, char **argv)
   trace_init();
 
   size = 1;
-  
+
 
   while(1) {
-	
-    if (!size) {       /* no more instructions (trace_items) to simulate */
+
+    if (!size && total_latency == 0) {       /* no more instructions (trace_items) to simulate */
 	  pipe_occupancy--;
 	  tr_entry = &noop;
 	  if (pipe_occupancy == 0) {
@@ -149,14 +151,14 @@ int main(int argc, char **argv)
 
 		size = trace_get_item(&tr_entry);
 
-    } 
-	cycle_number++;	
+    }
+	cycle_number++;
 	if(num_squash == 0)
 		squashed = NO_HAZ;
 	stalled = NO_HAZ;
-	
-	/** 
-	 **Check for cache access first to see if there is any pipeline stalling, and calculate total latency for a single instruction or data access. 
+
+	/**
+	 **Check for cache access first to see if there is any pipeline stalling, and calculate total latency for a single instruction or data access.
 	 **Check for stalling in MEM1 (L1 Data Cache) first, followed by IF1 (L1 Instruction Cache) if there is no stalling in MEM1
 	 **Resolve current stalling entirely before moving on to next case of stalling
 	 **If stalling in L1 Data cache is detected while waiting for L1 Instruction cache, stall until first data access is resolved
@@ -164,41 +166,49 @@ int main(int argc, char **argv)
 	 if(total_latency == 0)
 	 {
 		 //printf("Cache_delay: %d\n", cache_delay);
-		 cache_delay = NO_HAZ;
-		 if (pipeline[4]->type == ti_LOAD || pipeline[4]->type == ti_STORE) {
-			 if (pipeline[4]->type == ti_LOAD) {
-				 total_latency = cache_access(L1_D, L2, pipeline[4]->Addr, 0); 		//checks for an L1_D miss
-			 } else {
-				 total_latency = cache_access(L1_D, L2, pipeline[4]->Addr, 1); 		//checks for an L1_D miss
-			 }
-			 if (total_latency == 0) {
-				 L1_D_hits++;
-			 }
-			 L1_D_accesses++;
-		 }
-		 
-		 if(total_latency == 0)			
+		 if (cache_delay != NO_HAZ)
 		 {
-
-			 total_latency = cache_access(L1_I, L2, pipeline[0]->Addr, 0);		//no L1_D miss, so checks for L1_I miss
-			 L1_I_accesses++;
-			 if(total_latency != 0) {
-				cache_delay = INST_ACC;												//confirmed L1_I miss
-				L1_I_misses++;
-			 } else {
-				L1_I_hits++;
-			 }
-				
+		 	cache_delay = NO_HAZ;
 		 }
 		 else {
-			 cache_delay = DATA_ACC;													//confirmed L1_D miss
-			 L1_D_misses++;
+			 if (pipeline[4]->type == ti_LOAD || pipeline[4]->type == ti_STORE) {
+				 if (pipeline[4]->type == ti_LOAD) {
+					 total_latency = cache_access(L1_D, L2, pipeline[4]->Addr, 0); 		//checks for an L1_D miss
+				 } else {
+					 total_latency = cache_access(L1_D, L2, pipeline[4]->Addr, 1); 		//checks for an L1_D miss
+				 }
+				 if (total_latency == 0) {
+					 L1_D_hits++;
+				 }
+				 L1_D_accesses++;
+			 }
+
+			 if(total_latency == 0)
+			 {
+				 if (pipeline[0]->dReg != 255 && pipeline[0]->PC != I_inst_checked) //check for no-op and if the instruction was just checked
+				 {
+					 total_latency = cache_access(L1_I, L2, pipeline[0]->PC, 0);		//no L1_D miss, so checks for L1_I miss
+					 L1_I_accesses++;
+					 I_inst_checked = pipeline[0]->PC;
+					 if(total_latency != 0) {
+						cache_delay = INST_ACC;												//confirmed L1_I miss
+						L1_I_misses++;
+					 } else {
+						L1_I_hits++;
+					 }
+			 	}
+
+			 }
+			 else {
+				 cache_delay = DATA_ACC;													//confirmed L1_D miss
+				 L1_D_misses++;
+			 }
 		 }
 	 } else {
 		 if(cache_delay == INST_ACC && (pipeline[4]->type == ti_LOAD || pipeline[4]->type == ti_STORE) && cache_access(L1_D, L2, pipeline[4]->Addr, accesstype) != 0) {
-			 cache_delay = DATA_ACC;														//handles unique scenario where stalling in L1 Instruction cache, but then 
+			 cache_delay = DATA_ACC;														//handles unique scenario where stalling in L1 Instruction cache, but then
 		 }																		//  detects another stalling in L1 Data cahce before finished accessing instruction
-	 } 
+	 }
 	/**
 	 **Check for hazards here. Suggested order is structural hazards then data hazards.
 	 **If there are both, fixing only the data hazard should fix both, so setting it second
@@ -206,70 +216,70 @@ int main(int argc, char **argv)
 	 **Branch is kinda weird because it doesn't stall really so that might end up being its
 	 **own thing.
 	 **/
-	 
+
 	 //structural
 	 tempWB = pipeline[6]->type;
 	 tempID = pipeline[2]->type;
-	 
+
 	 //check if the WB stage is writing to the register file: i.e. is an RType, IType, or Load instruction
 	 //also check if the ID stage is reading from the register file: i.e. is an RType, Load, Store, Branch, JRType, or IType (as long as sReg_a is used) instruction
-	 if ((tempWB == ti_RTYPE) || (tempWB == ti_ITYPE) || (tempWB == ti_LOAD))	
+	 if ((tempWB == ti_RTYPE) || (tempWB == ti_ITYPE) || (tempWB == ti_LOAD))
 	 {
-		 if((tempID == ti_RTYPE) || ((tempID == ti_ITYPE) && (pipeline[2]->sReg_a != 255)) || (tempID == ti_LOAD) 
-		   || (tempID == ti_STORE) || (tempID == ti_BRANCH) || (tempID == ti_JRTYPE)) 
+		 if((tempID == ti_RTYPE) || ((tempID == ti_ITYPE) && (pipeline[2]->sReg_a != 255)) || (tempID == ti_LOAD)
+		   || (tempID == ti_STORE) || (tempID == ti_BRANCH) || (tempID == ti_JRTYPE))
 			{
 			 stalled = STRUCT_HAZ;
 			}
 	 }
-	 
+
 	 //data
 	 data_haz_type = 0;
-	 
-	 if (pipeline[3]->type == ti_LOAD) 
+
+	 if (pipeline[3]->type == ti_LOAD)
 	 {
-		 if (pipeline[2]->type == ti_RTYPE || pipeline[2]->type == ti_STORE || pipeline[2]->type == ti_BRANCH) 
+		 if (pipeline[2]->type == ti_RTYPE || pipeline[2]->type == ti_STORE || pipeline[2]->type == ti_BRANCH)
 		 {
-			 if ((pipeline[3]->dReg == pipeline[2]->sReg_a) || (pipeline[3]->dReg == pipeline[2]->sReg_b)) 
-			 {
-				 stalled = DATA_HAZ;
-				 data_haz_type = 0;
-			 } 
-		 } 
-		 else if (pipeline[2]->type == ti_ITYPE || pipeline[2]->type == ti_LOAD || pipeline[2]->type == ti_JRTYPE)
-		 { 
-			 if (pipeline[3]->dReg == pipeline[2]->sReg_a) 
+			 if ((pipeline[3]->dReg == pipeline[2]->sReg_a) || (pipeline[3]->dReg == pipeline[2]->sReg_b))
 			 {
 				 stalled = DATA_HAZ;
 				 data_haz_type = 0;
 			 }
-		 }  
-	 } 
-	 
-	 if (pipeline[4]->type == ti_LOAD) 
+		 }
+		 else if (pipeline[2]->type == ti_ITYPE || pipeline[2]->type == ti_LOAD || pipeline[2]->type == ti_JRTYPE)
+		 {
+			 if (pipeline[3]->dReg == pipeline[2]->sReg_a)
+			 {
+				 stalled = DATA_HAZ;
+				 data_haz_type = 0;
+			 }
+		 }
+	 }
+
+	 if (pipeline[4]->type == ti_LOAD)
 	 {
 		 if (pipeline[2]->type == ti_RTYPE || pipeline[2]->type == ti_STORE || pipeline[2]->type == ti_BRANCH)
-		 {	 
-			 if ((pipeline[4]->dReg == pipeline[2]->sReg_a) || (pipeline[4]->dReg == pipeline[2]->sReg_b)) 
+		 {
+			 if ((pipeline[4]->dReg == pipeline[2]->sReg_a) || (pipeline[4]->dReg == pipeline[2]->sReg_b))
 			 {
 				 stalled = DATA_HAZ;
 				 data_haz_type = 1;
-			 } 
-		 } 
-		 else if (pipeline[2]->type == ti_ITYPE || pipeline[2]->type == ti_LOAD || pipeline[2]->type == ti_JRTYPE) 
-		 {			 
+			 }
+		 }
+		 else if (pipeline[2]->type == ti_ITYPE || pipeline[2]->type == ti_LOAD || pipeline[2]->type == ti_JRTYPE)
+		 {
 			 if (pipeline[4]->dReg == pipeline[2]->sReg_a) {
 				 stalled = DATA_HAZ;
 				 data_haz_type = 1;
 			 }
-		 } 
-	 } 
-	 
+		 }
+	 }
+
 	 //branch
      if (pipeline[0]->type == ti_BRANCH) {
 	   branch_taken = pipeline[0]->Addr == tr_entry->PC;
 	   index = (pipeline[0]->Addr >> 3 ) % hash_table_size; //if you want bits 8-3 should this be left 24 and right 27? probably...gonna use mod to pick bits now
 	   if (prediction_method == 0) {
-		   
+
 		   if (pipeline[0]->Addr == tr_entry->PC) {//can this condition just be if(branch_taken).  Yes, i think its form before i made the 'branch_taken' variable
 			   squashed = CONT_HAZ;
 			   num_squash = 3;
@@ -288,10 +298,10 @@ int main(int argc, char **argv)
 			   bp_hash_table[index][1] = branch_taken;
 			   if (branch_taken) {
 				   squashed = CONT_HAZ;
-				   num_squash = 3;				   
+				   num_squash = 3;
 			   }
 		   }
-		   
+
 	   }
 	   else if (prediction_method == 2) {
 		   if (bp_hash_table[index][0] == pipeline[0]->Addr) {
@@ -331,19 +341,19 @@ int main(int argc, char **argv)
 			   bp_hash_table[index][1] = branch_taken*3;
 			   if (branch_taken) {
 				   squashed = CONT_HAZ;
-				   num_squash = 3;				   
-			   }			   
+				   num_squash = 3;
+			   }
 		   }
 	   }
 	 }
 
     /**
 	--From the assignment (page 3)
-	The project is to replace the simple single cycle simulation with a simulation of the 7-stages pipeline 
-	which will also output the total number of execution cycles as well as the instruction that exits the pipeline 
-	in each cycle (if the switch trace_view_on is set to 1). 
+	The project is to replace the simple single cycle simulation with a simulation of the 7-stages pipeline
+	which will also output the total number of execution cycles as well as the instruction that exits the pipeline
+	in each cycle (if the switch trace_view_on is set to 1).
 	*/
-	
+
     if (trace_view_on) {/* print the executed instruction if trace_view_on=1	*/
 	  switch(pipeline[6]->type) {
 		case ti_NOP:
@@ -361,11 +371,11 @@ int main(int argc, char **argv)
 		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
 		  break;
 		case ti_LOAD:
-		  printf("[cycle %d] LOAD:",cycle_number) ;      
+		  printf("[cycle %d] LOAD:",cycle_number) ;
 		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->dReg, pipeline[6]->Addr);
 		  break;
 		case ti_STORE:
-		  printf("[cycle %d] STORE:",cycle_number) ;      
+		  printf("[cycle %d] STORE:",cycle_number) ;
 		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", pipeline[6]->PC, pipeline[6]->sReg_a, pipeline[6]->sReg_b, pipeline[6]->Addr);
 		  break;
 		case ti_BRANCH:
@@ -377,7 +387,7 @@ int main(int argc, char **argv)
 		  printf(" (PC: %x)(addr: %x)\n", pipeline[6]->PC,pipeline[6]->Addr);
 		  break;
 		case ti_SPECIAL:
-		  printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
+		  printf("[cycle %d] SPECIAL:\n",cycle_number) ;
 		  break;
 		case ti_JRTYPE:
 		  printf("[cycle %d] JRTYPE:",cycle_number) ;
@@ -385,12 +395,12 @@ int main(int argc, char **argv)
 		  break;
 	  }
     }
-	//printf("Latency %u in cycles and then the cache access type %d\n", total_latency, cache_delay);
+	//printf("Latency %u in cycles and I or D %d\n", total_latency, cache_delay);
 
 	//Pipeline advancing loop
 	for (i = 6; i >= 1; i = i - 1)
 	{
-		/** 
+		/**
 		 **insert no-ops as needed below
 		 **/
 		if(i==5) 				/* check for Data Access stalling */
@@ -402,7 +412,7 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
-		
+
 		if(i==1)				/* check for Instruction Access stalling */
 		{
 			if(cache_delay == INST_ACC)
@@ -412,7 +422,7 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
-		 
+
 		if ((i == 3)) {        /*hazard with writing to the register file*/
 			if(stalled == STRUCT_HAZ)
 			{
@@ -425,14 +435,14 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
-		
+
 		else if ((i==4) && (stalled == DATA_HAZ)) {	/*hazard when instruction expects data that is still being loaded*/
 			if (data_haz_type == 1) {
 				pipeline[4] = &noop;
 				break;
 			}
 		}
-		
+
 		pipeline[i] = pipeline[i - 1];
 	}
 	if(cache_delay == NO_HAZ && squashed == CONT_HAZ && !stalled) {		/*hazard when branches are incorrectly predicted, must have no cache misses and no other hazards*/
@@ -442,7 +452,7 @@ int main(int argc, char **argv)
 	else if(!stalled && cache_delay == NO_HAZ) {
 		pipeline[0] = tr_entry;
 	}
-	
+
 	if (total_latency != 0) {
 			total_latency--;
 		}
--- project2/cache.h
+++ project2/cache.h
@@ -7,7 +7,7 @@ unsigned int L2_hits = 0;
 unsigned int L2_misses = 0;
 
 
-struct cache_blk_t { // note that no actual data will be stored in the cache 
+struct cache_blk_t { // note that no actual data will be stored in the cache
   unsigned long tag;
   char valid;
   char dirty;
@@ -15,19 +15,19 @@ struct cache_blk_t { // note that no actual data will be stored in the cache
 };
 
 struct cache_t {
-	// The cache is represented by a 2-D array of blocks. 
+	// The cache is represented by a 2-D array of blocks.
 	// The first dimension of the 2D array is "nsets" which is the number of sets (entries)
 	// The second dimension is "assoc", which is the number of blocks in each set.
   int nsets;					// number of sets
   int blocksize;				// block size
   int assoc;					// associativity
   int mem_latency;				// the miss penalty
-  
+
   //-------------------------------------------------------------------------------------------
   //unsigned int accesses;
   //unsigned int hits;
   //unsigned int misses;
-  
+
   struct cache_blk_t **blocks;	// a pointer to the array of cache blocks
 };
 
@@ -35,11 +35,11 @@ struct cache_t * cache_create(int size, int blocksize, int assoc, int mem_latenc
 {
   int i, nblocks , nsets ;
   struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
-		
+
   nblocks = size *1024 / blocksize ;// number of blocks in the cache
   nsets = nblocks / assoc ;			// number of sets (entries) in the cache
   C->blocksize = blocksize ;
-  C->nsets = nsets  ; 
+  C->nsets = nsets  ;
   C->assoc = assoc;
   C->mem_latency = mem_latency;
 
@@ -55,9 +55,9 @@ struct cache_t * cache_create(int size, int blocksize, int assoc, int mem_latenc
 int updateLRU(struct cache_t *cp ,int index, int way)
 {
 	int k ;
-	for (k=0 ; k< cp->assoc ; k++) 
+	for (k=0 ; k< cp->assoc ; k++)
 	{
-	  if(cp->blocks[index][k].LRU < cp->blocks[index][way].LRU) 
+	  if(cp->blocks[index][k].LRU < cp->blocks[index][way].LRU)
 		 cp->blocks[index][k].LRU = cp->blocks[index][k].LRU + 1 ;
 	}
 	cp->blocks[index][way].LRU = 0 ;
@@ -65,8 +65,8 @@ int updateLRU(struct cache_t *cp ,int index, int way)
 
 int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, int access_type /*0 for read, 1 for write*/)
 {
-	
-	
+
+
 	int flag = 0;
 	int i,latency ;
 	//L1
@@ -77,10 +77,10 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 	int L2_block_address ;
 	int L2_index ;
 	int L2_tag ;
-	
+
 	int way ;
 	int max ;
-	
+
 	//Extras for rehashing to keep "inclusive"
 	unsigned long rehash_address;
 	int temp_block_address;
@@ -91,7 +91,7 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 	L1_block_address = (address / L1->blocksize);
 	L1_tag = L1_block_address / L1->nsets;
 	L1_index = L1_block_address - (L1_tag * L1->nsets) ;
-	
+
 	if (L2->nsets != 0) {
 		L2_block_address = (address / L2->blocksize);
 		L2_tag = L2_block_address / L2->nsets;
@@ -99,7 +99,7 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 	}
 
 	latency = 0;
-	
+  //printf("\naccessing address %lx index %d and tag %d", address, L1_index, L1_tag);
 	//L1 CACHE HIT------------------------------------------------------------------------------------------------
 	for (i = 0; i < L1->assoc; i++) {	/* look for the requested block */
 		if (L1->blocks[L1_index][i].tag == L1_tag && L1->blocks[L1_index][i].valid == 1) {
@@ -110,7 +110,7 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 			return(latency);					/* a cache hit */
 		}
 	}
-	
+
 	//L1 CACHE MISS------------------------------------------------------------------------------------------------
 	/* a cache miss */
 	for (way=0 ; way < L1->assoc ; way++){		/* look for an invalid entry */
@@ -118,21 +118,24 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 			latency = latency + L1->mem_latency;	/* account for reading the block from memory*/
 													/* should instead read from L2, in case you have an L2 */
 			if (L2->nsets != 0) {
-				L2_accesses++;														
+				L2_accesses++;
 			}
 			L1->blocks[L1_index][way].valid = 1 ;
 		    L1->blocks[L1_index][way].tag = L1_tag ;
-		    updateLRU(L1, L1_index, way); 
+		    updateLRU(L1, L1_index, way);
 		    L1->blocks[L1_index][way].dirty = 0;
-			
+
 		    if(access_type == 1) L1->blocks[L1_index][way].dirty = 1 ;
-			
+
 			flag = 1;//return(latency);				/* an invalid entry is available*/
+      printf("\nfound an empty spot for addr %d", address);
+      break;
 		}
 	}
-	
+
 	//Only execute if we couldn't find an open space
 	if (flag == 0) {
+    printf("\nevicting for address %d", address);
 		max = L1->blocks[L1_index][0].LRU ;	/* find the LRU block */
 		way = 0 ;
 		for (i=1 ; i< L1->assoc ; i++) {
@@ -140,12 +143,12 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 				max = L1->blocks[L1_index][i].LRU ;
 				way = i ;
 			}
-		} 
-		
+		}
+
 		if (L1->blocks[L1_index][way].dirty == 1) {
 			latency = latency + L1->mem_latency;	/* for writing back the evicted block */ ///also set the L2 location to dirty
-		} 
-		
+		}
+
 		latency = latency + L1->mem_latency;		/* for reading the block from memory*/
 													/* should instead write to and/or read from L2, in case you have an L2 */
 		L1->blocks[L1_index][way].tag = L1_tag ;
@@ -153,7 +156,7 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 		L1->blocks[L1_index][i].dirty = 0 ;
 		if(access_type == 1) L1->blocks[L1_index][i].dirty = 1 ;
 	}
-	
+
 	if (L2->nsets != 0) {
 		//L2 HIT------------------------------------------------------------------------------------------------
 		for (i = 0; i < L2->assoc; i++) {	/* look for the requested block */
@@ -166,27 +169,27 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 				return(latency);					/* a cache hit */ //IF THIS IS HIT THEN WE DONT RETURN LATENCY BUT JUMP TO L1 MISS INFO
 			}
 		}
-		
+
 		//L2 MISS------------------------------------------------------------------------------------------------
 		for (way=0 ; way < L2->assoc ; way++){		/* look for an invalid entry */
 			if (L2->blocks[L2_index][way].valid == 0) {
 				latency = latency + L2->mem_latency;	/* account for reading the block from memory*/
 														/* should instead read from L2, in case you have an L2 */
-				L2_misses++;										
+				L2_misses++;
 				L2->blocks[L2_index][way].valid = 1 ;
 				L2->blocks[L2_index][way].tag = L2_tag ;
-				updateLRU(L2, L2_index, way); 
+				updateLRU(L2, L2_index, way);
 				L2->blocks[L2_index][way].dirty = 0;
-				
+
 				if(access_type == 1) L2->blocks[L2_index][way].dirty = 1 ;
-				
+
 				return(latency);				/* an invalid entry is available*/ //JUMP TO L1 MISS
 			}
 		}
-		
+
 		max = L2->blocks[L2_index][0].LRU ;	/* find the LRU block */
 		way = 0 ;
-		
+
 		for (i=1 ; i< L2->assoc ; i++) {
 			//Find LRU in L2
 			if (L2->blocks[L2_index][i].LRU > max) {
@@ -203,16 +206,16 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 						way = i ;
 					}
 				}
-				
+
 			}
-		} 
-		   
-		
+		}
+
+
 		//If dirty in L2, WRITE BACK
 		if (L2->blocks[L2_index][way].dirty == 1) {
 			latency = latency + L2->mem_latency;
-		} 
-		
+		}
+
 		L2_misses++;
 		latency = latency + L2->mem_latency;		/* for reading the block from memory*/
 													/* should instead write to and/or read from L2, in case you have an L2 */
@@ -221,10 +224,9 @@ int cache_access(struct cache_t *L1, struct cache_t *L2, unsigned long address, 
 		L2->blocks[L2_index][i].dirty = 0 ;
 		if(access_type == 1) L2->blocks[L2_index][i].dirty = 1 ;
 	}
-	
-	
+
+
 	//------------------------------------------------------------------------------------------------------------
-	
+
 	return(latency) ;
 }
-
